b7003fc848fcad41f6f98c1be101d63137eb5e39
voliev <voliev82@gmail.com>
2019-12-10 21:58:25 -0600

    Updates

---
+++ precache-manifest.83d0b2fd13ce802a37acd0ddd7d92b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f110f601b4c223253632",
+    "url": "./static/js/main.f110f601.chunk.js"
+  },
+  {
+    "revision": "f4d559c9ea34dfac5797",
+    "url": "./static/js/1.f4d559c9.chunk.js"
+  },
+  {
+    "revision": "f110f601b4c223253632",
+    "url": "./static/css/main.3254a9e3.chunk.css"
+  },
+  {
+    "revision": "f4d559c9ea34dfac5797",
+    "url": "./static/css/1.0012adc8.chunk.css"
+  },
+  {
+    "revision": "14e8e51c19a35b0b3207f0f51b441544",
+    "url": "./index.html"
+  }
+];
